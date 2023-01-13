2b8aa49b2865ac4774c06b88dae58fcc89bb80e3
jay-gami <jay.gami@softices.com>
2023-01-13 18:18:41 +0530

    js task 3

---
+++ js/task3.js
@@ -0,0 +1,28 @@
+function add(){
+
+
+
+let table = document.getElementById("click");
+   
+
+let row = table.insertRow(); 
+
+let c1 = row.insertCell(0);
+let c2 = row.insertCell(1);
+let c3 = row.insertCell(2);
+let c4 = row.insertCell(3);
+let c5 = row.insertCell(4);
+let c6 = row.insertCell(5);
+
+c1.innerText = "Jay"
+c2.innerText = "50"
+c3.innerText = "30"
+c4.innerText="54"
+c5.innerText="120"
+c6.innerText="75%"
+
+
+
+   
+}
+ 
