ec3c234c53d8d3e241ea11adf0b7646ae776aefd
jay-gami <jay.gami@softices.com>
2023-01-13 13:14:41 +0530

    new

--- color.js
+++ color.js
@@ -2,29 +2,42 @@
 function red() {
   // const a = document.getElementById("red");
   document.body.style.backgroundColor = "red";
+
   document.getElementById("contact").style.backgroundColor = "red";
+  document.getElementById("verbar").style.backgroundColor = "black";
+
 }
 
 function Black() {
 //   const a = document.getElementById("Black");
   document.body.style.backgroundColor = "black";
+
   document.getElementById("contact").style.backgroundColor = "black";
+  document.getElementById("verbar").style.backgroundColor = "violet";
+
 }
 
 function Orange() {
 //   const a = document.getElementById("Orange");
   document.body.style.backgroundColor = "Orange";
+
   document.getElementById("contact").style.backgroundColor = "orange";
+  document.getElementById("verbar").style.backgroundColor = "black";
+
 }
 
 function Purple() {
 //   const a = document.getElementById("Purple");
   document.body.style.backgroundColor = "Purple";
   document.getElementById("contact").style.backgroundColor = "purple";
+  document.getElementById("verbar").style.backgroundColor = "black";
+
+
 }
 
 function Violet() {
 //   const a = document.getElementById("Violet");
   document.body.style.backgroundColor = "Violet";
   document.getElementById("contact").style.backgroundColor = "violet";
+  document.getElementById("verbar").style.backgroundColor = "black";
 }
