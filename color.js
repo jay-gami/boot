0d6dde77c995369441d631e538fd97e9adb8fcdb
jay-gami <jay.gami@softices.com>
2023-01-13 12:58:36 +0530

    new js

--- color.js
+++ color.js
@@ -1,3 +1,30 @@
-function red(){
-    
+
+function red() {
+  // const a = document.getElementById("red");
+  document.body.style.backgroundColor = "red";
+  document.getElementById("contact").style.backgroundColor = "red";
+}
+
+function Black() {
+//   const a = document.getElementById("Black");
+  document.body.style.backgroundColor = "black";
+  document.getElementById("contact").style.backgroundColor = "black";
+}
+
+function Orange() {
+//   const a = document.getElementById("Orange");
+  document.body.style.backgroundColor = "Orange";
+  document.getElementById("contact").style.backgroundColor = "orange";
+}
+
+function Purple() {
+//   const a = document.getElementById("Purple");
+  document.body.style.backgroundColor = "Purple";
+  document.getElementById("contact").style.backgroundColor = "purple";
+}
+
+function Violet() {
+//   const a = document.getElementById("Violet");
+  document.body.style.backgroundColor = "Violet";
+  document.getElementById("contact").style.backgroundColor = "violet";
 }
