811fba1d893addc4ed73c463e1484fcfd02e3c05
jay-gami <jay.gami@softices.com>
2023-01-19 09:28:15 +0530

    nw

--- js/task3.js
+++ js/task3.js
@@ -1,11 +1,11 @@
 var sum=0;
 
-function one(){
+function AddNewRow(){
   let h2 = document.getElementById("click");
 
 sum++;
 
-  var table=`<tr><td id="roll">${sum}</td><td><input type="text" id="txtname" onchange="add()"></td><td><input type="number" id="txtmaths" onchange="add()" ></td><td><input type="number" id="txtenglish" onchange="add()" ></td><td><input type="number" id="txtscience" onchange="add()"></td><td id="total"></td><td id="percentage"></td></tr>`
+  var table=`<tr><td id="roll">${sum}</td><td><input type="text" id="txtname" onchange="AddNewData()"></td><td><input type="number" id="txtmaths" onchange=""AddNewData(this)" ></td><td><input type="number" id="txtenglish" onchange="AddNewData()" ></td><td><input type="number" id="txtscience" onchange="AddNewData()"></td><td id="total"></td><td id="percentage"></td></tr>`
     h2.insertAdjacentHTML("beforeend",table);  
 
 
@@ -14,14 +14,14 @@ sum++;
 
 
 
-function add() {
-  
+function AddNewData() {
+
   var maths=document.getElementById("txtmaths").value;
   var english=document.getElementById("txtenglish").value;
   var science=document.getElementById("txtscience").value;
 
   var total =Number(maths)+Number(english)+Number(science);
-  document.getElementById("total").innerHTML=(total);
+  document.getElementById("total").innerHTML=total;
 
   var percentage =Number(total/300)*100;
   document.getElementById("percentage").innerHTML=percentage;
