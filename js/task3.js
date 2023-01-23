b2357391035d870387f485a64a364b1f45444ce1
jay-gami <jay.gami@softices.com>
2023-01-23 09:31:17 +0530

    adding now succcessfully

--- js/task3.js
+++ js/task3.js
@@ -1,17 +1,19 @@
 var sum=0;
-
+var i=0;
 
 
 function AddNewRow(){
   let h2 = document.getElementById("click");
-
-
-  
 sum++;
-
-
-  var table=`<tr><td id="roll">${sum}</td><td><input type="text" id="txtname" ></td><td><input type="number" id="txtmaths" onchange="AddNewData()" ></td><td><input type="number" id="txtenglish" onchange="AddNewData()" ></td><td><input type="number" id="txtscience" onchange="AddNewData()"></td><td id="total"></td>
- <td id="percentage"></td></tr>`
+i++;
+  var table=`<tr >
+  <td id="roll">${sum}</td><td><input type="text" id="txtname${i}"  onchange="AddNewData(${i})"></td>
+  <td><input type="number" id="txtmaths${i}" onchange="AddNewData(${i})" ></td>
+  <td><input type="number" id="txtenglish${i}" onchange="AddNewData(${i})" ></td>
+  <td><input type="number" id="txtscience${i}" onchange="AddNewData(${i})"></td>
+  <td id="total${i}"></td>
+  <td id="percentage${i}"></td>
+ </tr>`
     h2.insertAdjacentHTML("beforeend",table);  
 
 
@@ -20,21 +22,23 @@ sum++;
 
 
 
-function AddNewData() {
-  
-  var maths=document.getElementById("txtmaths").value;
-  var english=document.getElementById("txtenglish").value;
-  var science=document.getElementById("txtscience").value;
+function AddNewData(index) {
+
+  var j;
+  for(j=1;j<=index;j++){
+  var maths=document.getElementById("txtmaths"+j).value;
+  var english=document.getElementById("txtenglish"+j).value;
+  var science=document.getElementById("txtscience"+j).value;
 
   var total =Number(maths)+Number(english)+Number(science);
 
-  document.getElementById("total").innerHTML=total;
+  document.getElementById("total"+j).innerHTML=total;
 
   var percentage =Number(total/300)*100;
-  document.getElementById("percentage").innerHTML=percentage;
+  document.getElementById("percentage"+j).innerHTML=percentage;
 
  
-
+  }
 }
 
 
