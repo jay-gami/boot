f77048fee25bb351a97035bb155c5a044e0a2369
jay-gami <jay.gami@softices.com>
2023-01-31 11:14:52 +0530

    new

--- js/task3.js
+++ js/task3.js
@@ -57,7 +57,7 @@ $(document).ready(function () {
     }
   });
 
-  $("tbody").on("click", ".delete", function () {
+  $(".delete  ").on("click", ".delete", function () {
     $(this).closest("tr").remove();
 
     sum--;
