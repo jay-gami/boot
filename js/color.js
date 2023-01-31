14cceba012e09fbac41ce26ca2f872fe1b624d70
jay-gami <jay.gami@softices.com>
2023-01-31 17:34:39 +0530

    added and delete

--- js/color.js
+++ js/color.js
@@ -1,44 +1,36 @@
-
 function red() {
   // const a = document.getElementById("red");
   document.body.style.backgroundColor = "red";
 
   document.getElementById("contact").style.backgroundColor = "red";
-  document.getElementById("verbar").style.backgroundColor = "black";
-
+  document.getElementById("contact").style.color = "purple";
 }
 
 function Black() {
-//   const a = document.getElementById("Black");
+  const a = document.getElementById("Black");
   document.body.style.backgroundColor = "black";
 
   document.getElementById("contact").style.backgroundColor = "black";
-  document.getElementById("verbar").style.backgroundColor = "violet";
-
 }
 
 function Orange() {
-//   const a = document.getElementById("Orange");
+  //   const a = document.getElementById("Orange");
   document.body.style.backgroundColor = "Orange";
 
   document.getElementById("contact").style.backgroundColor = "orange";
   document.getElementById("verbar").style.backgroundColor = "black";
-
 }
 
 function Purple() {
-//   const a = document.getElementById("Purple");
+  //   const a = document.getElementById("Purple");
   document.body.style.backgroundColor = "Purple";
   document.getElementById("contact").style.backgroundColor = "purple";
   document.getElementById("verbar").style.backgroundColor = "black";
-
-
 }
 
 function Violet() {
-//   const a = document.getElementById("Violet");
+  //   const a = document.getElementById("Violet");
   document.body.style.backgroundColor = "Violet";
   document.getElementById("contact").style.backgroundColor = "violet";
   document.getElementById("verbar").style.backgroundColor = "black";
 }
-
